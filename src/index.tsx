836a0bf5f09e4fe802320827d1f15bb8fef363fc
Николай <vasilievkolay@yandex.ru>
2023-10-15 15:29:41 +0300

    Исправлены ошибки

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import LoginAndPassword from './components/app/app';
-import FavoritesScreen from './components/app/app';
-import OfferScreen from './pages/offer-screen/offer-screen';
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const Settings = {
-  sum : 0,
-  email: "Oliver.conner@gmail.com",
-  password: "12345678"
+const settings = {
+  sum: 0,
+  email: 'Oliver.conner@gmail.com',
+  password: '12345678',
 } as const;
 
 root.render(
   <React.StrictMode>
-    <App
-      sum = {Settings.sum}
-    />
-    <LoginAndPassword
-      email = {Settings.email}
-      password = {Settings.password}
-    />
-    <FavoritesScreen />
-    <OfferScreen/>
+    <App sum={settings.sum} />
   </React.StrictMode>
 );
